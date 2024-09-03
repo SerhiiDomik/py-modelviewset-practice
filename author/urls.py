5e5c99077a432e5251fdd27491134bd3221542d3
Serhii <serhii.domik@gmail.com>
2024-09-03 17:49:03 +0300

    fixed?

--- author/urls.py
+++ author/urls.py
@@ -1,12 +1,29 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
 from author.views import AuthorViewSet
 
-app_name = 'author'
+# router = DefaultRouter()
+# router.register(r'authors', AuthorViewSet, basename='author')
+#
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
 
-router = DefaultRouter()
-router.register(r'authors', AuthorViewSet, basename='author')
+app_name = "author"
+
+author_list = AuthorViewSet.as_view(actions={"get": "list", "post": "create"})
+
+author_detail = AuthorViewSet.as_view(
+    actions={
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy",
+    }
+)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("actors/", author_list, name="actor_list"),
+    path("actors/<int:pk>/", author_detail, name="actor_detail"),
 ]
